7b4d7c314544c223aaed8c897d611ef7ded9526f
冯文轩 <moxixi>
2022-07-10 16:12:26 +0800

    feat: update

--- text-area/src/renderer/pages/dashboard/text/index.tsx
+++ text-area/src/renderer/pages/dashboard/text/index.tsx
@@ -1,22 +1,35 @@
+import { useRef } from 'react';
+import { marked } from 'marked';
 import { Input } from 'antd';
 import './text.css';
 
 const { TextArea } = Input;
 
 const TextEditor = () => {
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const showRef = useRef<HTMLDivElement>(null);
+
+  const handleTransformData = (event) => {
+    const { value } = event.target;
+    // console.log(value, marked.parse(value), showRef.current);
+
+    (showRef.current as HTMLDivElement).innerHTML = marked.parse(value);
+  };
+
   return (
     <>
       <h3>字数统计</h3>
       <div className="text_editor">
-        <textarea name="text" className="text_textarea" />
-        {/* <TextArea
-      className="text_textarea"
-      allowClear
-      showCount
-      // placeholder="请输入文章"
-      // defaultValue="请输入文章"
-    /> */}
-        <div className="text_performance">1</div>
+        {/* <textarea name="text" className="text_textarea" /> */}
+        <TextArea
+          className="text_textarea"
+          allowClear
+          showCount
+          placeholder="请输入内容"
+          ref={inputRef}
+          onChange={handleTransformData}
+        />
+        <div ref={showRef} className="text_performance" />
       </div>
     </>
   );
